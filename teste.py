2cfb54aeec280677aaf6832ab5d1e415b6ec6282
rafael1717y <rafaelufjf@gmail.com>
2021-03-16 10:31:58 -0300

    First commit.

---
+++ teste.py
@@ -0,0 +1,16 @@
+"""Arquivo de testes"""
+from collections import namedtuple
+
+
+
+def soma(x: int, y: int) -> int:
+    """Função que soma dois números inteiros."""
+    return x + y;
+
+
+
+print(soma(1, 1))
+
+print("Rafael")
+
+r'r\d{4}\r\n'
